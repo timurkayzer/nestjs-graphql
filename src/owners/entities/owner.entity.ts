f6b6b1961664297a9a04fec32af808ed9ea7c71a
Timur Kaiser <t.kayzer@tengebank.uz>
2023-03-29 17:40:33 +0500

    added owners

---
+++ src/owners/entities/owner.entity.ts
@@ -0,0 +1,19 @@
+import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { Pet } from 'src/pets/pet.entity';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+
+@ObjectType()
+@Entity()
+export class Owner {
+  @Field(() => Int)
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Field()
+  @Column()
+  name: string;
+
+  @OneToMany(() => Pet, pet => pet.owner)
+  @Field(() => [Pet], { nullable: true })
+  pets?: Pet[];
+}
---
+++ src/owners/owners.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@nestjs/common';
+import { CreateOwnerInput } from './dto/create-owner.input';
+import { UpdateOwnerInput } from './dto/update-owner.input';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Owner } from './entities/owner.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class OwnersService {
+
+  constructor(
+    @InjectRepository(Owner) private ownerRepository: Repository<Owner>
+  ) { }
+
+  create(createOwnerInput: CreateOwnerInput) {
+    const newOwner = this.ownerRepository.create(createOwnerInput);
+    return this.ownerRepository.save(newOwner);
+  }
+
+  findAll() {
+    return this.ownerRepository.find();
+  }
+
+  findOne(id: number) {
+    return this.ownerRepository.findOneOrFail({ where: { id } });
+  }
+
+  update(id: number, updateOwnerInput: UpdateOwnerInput) {
+    return this.ownerRepository.update({ id }, updateOwnerInput);
+  }
+
+  remove(id: number) {
+    return this.ownerRepository.delete({ id });
+  }
+}
